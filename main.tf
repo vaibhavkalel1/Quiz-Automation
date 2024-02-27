f49ace44d5351c682556208183d0d065452982c6
vaibhavkalel1 <110541903+vaibhavkalel1@users.noreply.github.com>
2024-02-27 14:59:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-provider "aws" {
-    region = "us-east-2"
-}
+resource "aws_instance" "public_instance" {
+ ami           = var.ami
+ instance_type = var.instance_type
 
-variable "name" {
-    description = "Name the instance on deploy"
-}
-
-resource "aws_instance" "admin_frontend" {
-    ami = "ami-05fb0b8c1424f266b"
-    instance_type = "t2.micro"
-    key_name = "f2"
-
-    tags = {
-            Name = var.name
-    }
+ tags = {
+   Name = var.name_tag,
+ }
 }

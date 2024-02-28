c84593ad6927fc0b737c22bd0971dfb759bb33e5
vaibhavkalel1 <110541903+vaibhavkalel1@users.noreply.github.com>
2024-02-28 10:57:21 +0530

    Delete ouput.tf

--- ouput.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
- value       = aws_instance.public_instance.public_ip
- description = "Public IP Address of EC2 instance"
-}
-
-output "instance_id" {
- value       = aws_instance.public_instance.id
- description = "Instance ID"
-}
